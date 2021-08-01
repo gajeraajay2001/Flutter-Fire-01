26f0d2237b437ef2415fcbfbb9364fb749a4947b
Ajay <gajeraajay2001@gmail.com>
2021-08-01 11:45:52 +0530

    commited

--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -20,7 +20,7 @@ class _SecondPageState extends State<SecondPage> {
               await FirebaseAuth.instance.signOut();
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                  content: Text("Logout Successfully ..... "),
+                  content: Text("Logout Successfully..... "),
                 ),
               );
               Navigator.of(context)
